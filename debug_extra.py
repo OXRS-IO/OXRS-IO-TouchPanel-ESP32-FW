47fd4d2a88ec7f79f8c47b6acb2e9a1afb896794
moinmoin-sh <53935853+moinmoin-sh@users.noreply.github.com>
2023-05-15 10:37:42 +0200

    fix debug_extra.py for PIO V 6.1.7

--- debug_extra.py
+++ debug_extra.py
@@ -1,13 +1,17 @@
 Import("env")
 
 # --- Add custom macros for the ALL files which name contains "main.cpp"
+# important change https://github.com/godotengine/godot/issues/74725
+# CPPDEFINES nolonger a list, but deque
 def addUnixTimestamp(env, node):
-
+  # get a copy of the CPPDEFINES
+  mycppdefines = env["CPPDEFINES"].copy()
+  # add the unix time stamp
+  mycppdefines.append({"BUILD_TIMESTAMP" : "$UNIX_TIME"})
+ 
   return env.Object(
       node,
-      CPPDEFINES=env["CPPDEFINES"]
-      + [("BUILD_TIMESTAMP", "$UNIX_TIME")]
+      CPPDEFINES=mycppdefines
   )
-
 env.AddBuildMiddleware(addUnixTimestamp, "*\src\main.cpp")
 
